ecc95ce97aaa87736cce069dd85fe4466e8ba562
Chris Dusyk <chris.dusyk@gmail.com>
2017-02-01 11:23:07 -0600

    Revert "Merge pull request #2 from LordCheese/logging"
    This reverts commit cb9f17a74275f0f2c746cf4c8e6cbcbedaf86f9f.

--- src/XdtTransform/Impl/XdtTransformer.cs
+++ src/XdtTransform/Impl/XdtTransformer.cs
@@ -1,84 +1,74 @@
 ﻿using Microsoft.Web.XmlTransform;
 using System;
-using NLog;
 using System.Text;
 using System.Xml;
 
 namespace XdtTransform.Impl
 {
-	public class XdtTransformer : IXdtTransformer
-	{
-		private static IXdtTransformer current;
-		private static Logger logger = LogManager.GetCurrentClassLogger();
+    public class XdtTransformer : IXdtTransformer
+    {
+        private static IXdtTransformer current;
+        public static IXdtTransformer Current
+        {
+            get { return current ?? (current = new XdtTransformer()); }
+        }
 
-		public static IXdtTransformer Current
-		{
-			get { return current ?? (current = new XdtTransformer()); }
-		}
+        private XdtTransformer()
+        {
+            this.PreserveWhitespace = true;
+        }
 
-		private XdtTransformer()
-		{
-			this.PreserveWhitespace = true;
-		}
+        #region IXdtTransformer
 
-		#region IXdtTransformer
+        public Boolean PreserveWhitespace { get; set; }
 
-		public Boolean PreserveWhitespace { get; set; }
+        public String Transform(String sourceXml, String transformXml)
+        {
+            if (String.IsNullOrEmpty(sourceXml))
+            {
+                throw new ArgumentNullException("sourceXml");
+            }
+            if (String.IsNullOrEmpty(transformXml))
+            {
+                throw new ArgumentNullException("transformXml");
+            }
 
-		public String Transform(String sourceXml, String transformXml)
-		{
-			if (String.IsNullOrEmpty(sourceXml))
-			{
-				throw new ArgumentNullException("sourceXml");
-			}
-			if (String.IsNullOrEmpty(transformXml))
-			{
-				throw new ArgumentNullException("transformXml");
-			}
+            try
+            {
+                using (var doc = new XmlTransformableDocument())
+                {
+                    doc.PreserveWhitespace = this.PreserveWhitespace;
+                    doc.LoadXml(sourceXml);
 
-			try
-			{
-				using (var doc = new XmlTransformableDocument())
-				{
-					logger.Debug($"Beginning transformation");
+                    using (var xDoc = new XmlTransformation(transformXml, false, null))
+                    {
+                        if (xDoc.Apply(doc))
+                        {
+                            var builder = new StringBuilder();
+                            var xmlWriterSettings = new XmlWriterSettings
+                            {
+                                Indent = true,
+                                IndentChars = "  "
+                            };
+                            using (var xmlWriter = XmlTextWriter.Create(builder, xmlWriterSettings))
+                            {
+                                doc.WriteTo(xmlWriter);
+                            }
+                            return builder.ToString();
+                        }
+                        else
+                        {
+                            throw new XdtTransformException("Unable to transform");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new XdtTransformException("Unable to transform", ex);
+            }
+        }
 
-					doc.PreserveWhitespace = this.PreserveWhitespace;
-					doc.LoadXml(sourceXml);
-
-					using (var xDoc = new XmlTransformation(transformXml, false, null))
-					{
-						if (xDoc.Apply(doc))
-						{
-							var builder = new StringBuilder();
-							var xmlWriterSettings = new XmlWriterSettings
-							{
-								Indent = true,
-								IndentChars = "  "
-							};
-							using (var xmlWriter = XmlTextWriter.Create(builder, xmlWriterSettings))
-							{
-								doc.WriteTo(xmlWriter);
-							}
-
-							logger.Debug("Successfully transformed document");
-
-							return builder.ToString();
-						}
-						else
-						{
-							logger.Warn("Unable to transform - xDoc.Apply failed");
-							throw new XdtTransformException("Unable to transform");
-						}
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				logger.Error(ex, $"Unable to transform, exception: {ex.Message}");
-				throw new XdtTransformException($"Unable to transform, exception: {ex.Message}", ex);
-			}
-		}
-
-		#endregion IXdtTransformer
-	}
+        #endregion
+    }
 }
